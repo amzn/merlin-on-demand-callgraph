9c727272acd74ff2d7cf244078f23a24eb822a54
Daniel Tattersall <dtatters@amazon.com>
2023-02-23 14:11:05 -0800

    Add merlin evalution to the full evaluation script
    **Motivation:**
    
    Changing merlin to output information in JSON form so that it can be run
    and read by the full evaluation script.
    
    **Assumptions/Open issues:**
    
    * Merlin's default sinks are not working for any of the benchmarks
      except foxx-framework, hence why the test only runs on them
    
    * Merlin does not appear to be picking up nearly the edges that other
      tools are. It seems to be a problem with babel finding/importing other
      files, but it needs further investigation
    
    **Testing:**
    
    * npm run full-eval and manually checked the results
    * sbt run on merlin and manually checked the results
    
    **Issue:**
    
    N/A

--- scripts/evaluation/src/full-evaluation.ts
+++ scripts/evaluation/src/full-evaluation.ts
@@ -1,58 +1,28 @@
 import * as fs from "fs-extra";
 import * as path from "path";
 import { nodeProfTransformer } from './nodeprof-transformer';
-import { runDynCg, runJam, getGraalNodePath } from "./callgraphs";
-import { getDynCgsDir, getJamCgsDir, getBenchmarkMainFile } from "./benchmark-utils";
+import { runDynCg, runJam, runMerlin, getGraalNodePath } from "./callgraphs";
+import { getDynCgsDir, getJamCgsDir, getMerlinCgsDir, getBenchmarkMainFile } from "./benchmark-utils";
 
 interface BenchmarkResult {
   name: string,
   outputFile: string
 }
 
-const normalizeNodeProfOutput = (benchmark: BenchmarkResult): BenchmarkResult => {
-  const outputFile = path.resolve(path.dirname(benchmark.outputFile), `${benchmark.name}.json`);
-  console.log("Transforming output...");
-  nodeProfTransformer(benchmark.outputFile, outputFile);
-  console.log(`Final output written to ${outputFile}`);
-
-  return {...benchmark, outputFile: outputFile};
-}
-
-const generateNodeProfCallGraphs = (jamPath: string, graalNodeHome: string, benchmarks: string[]): BenchmarkResult[] => {
-  console.log("Generating Dynamic CGs");
-  const dynCallgraphOutDir = getDynCgsDir();
-  const generatedCallGraphs: BenchmarkResult[] = [];
-  fs.ensureDirSync(dynCallgraphOutDir);
-
-  for (const benchmark of benchmarks) {
-
-    const startFile = getBenchmarkMainFile(benchmark);
-    const rawOutputFilePath = path.resolve(dynCallgraphOutDir, `${benchmark}.raw`);
-
-    try {
-      const resultBuffer = runDynCg(jamPath, graalNodeHome, benchmark, rawOutputFilePath, startFile);
-      console.log(resultBuffer.toString());
-      generatedCallGraphs.push({ name: benchmark, outputFile: rawOutputFilePath});
-      
-    } catch (e) {
-      // if there's an error continue with remaining benchmarks but log the problem
-      console.error(e);
-    }
-  }
-
-  return generatedCallGraphs;
-};
-
-const generateJamCallGraphs = (jamPath: string, graalNodeHome: string, benchmarks: string[]): BenchmarkResult[] => {
-  const jamCallgraphOutDir = getJamCgsDir();
-  fs.ensureDirSync(jamCallgraphOutDir);
+const generateCallgraph = (
+  benchmarks: string[], 
+  callgraphOutputDir: string, 
+  outputFileSuffix: string, 
+  callgraphFunc: (benchmark: string, outputPath: string) => Buffer
+) => {
+  fs.ensureDirSync(callgraphOutputDir);
 
   const generatedCallGraphs: BenchmarkResult[] = [];
 
   for (const benchmark of benchmarks) {
     try {
-      const outputFile = path.resolve(jamCallgraphOutDir, `${benchmark}.json`);
-      const resultBuffer = runJam(jamPath, graalNodeHome, benchmark, outputFile /* remaining defaults are okay */);
+      const outputFile = path.resolve(callgraphOutputDir, `${benchmark}${outputFileSuffix}`);
+      const resultBuffer = callgraphFunc(benchmark, outputFile);
       console.log(resultBuffer.toString());
       generatedCallGraphs.push({ name: benchmark, outputFile: outputFile});
     } catch (e) {
@@ -62,11 +32,57 @@ const generateJamCallGraphs = (jamPath: string, graalNodeHome: string, benchmark
   }
 
   return generatedCallGraphs;
-}
+};
+
+const generateNodeProfCallGraphs = (jamPath: string, graalNodeHome: string, benchmarks: string[]): BenchmarkResult[] => {
+  console.log("Generating Dynamic CGs");
+  return generateCallgraph(benchmarks, getDynCgsDir(), ".raw", (benchmark: string, outputFile: string) => {
+    const startFile = getBenchmarkMainFile(benchmark);
+    return runDynCg(jamPath, graalNodeHome, benchmark, outputFile, startFile);
+  });
+};
+
+const generateJamCallGraphs = (jamPath: string, graalNodeHome: string, benchmarks: string[]): BenchmarkResult[] => {
+  console.log("Generating Jam CGs");
+  return generateCallgraph(benchmarks, getJamCgsDir(), ".json", (benchmark: string, outputFile: string) => {
+    return runJam(jamPath, graalNodeHome, benchmark, outputFile /* remaining defaults are okay */);
+  });
+};
+
+const generateMerlinCallGraphs = (benchmarks: string[]): BenchmarkResult[] => {
+  console.log("Generating Merlin CGs");
+  return generateCallgraph(benchmarks, getMerlinCgsDir(), ".raw", (benchmark: string, outputFile: string) => {
+    return runMerlin(benchmark, outputFile);
+  });
+};
+
+const normalizeNodeProfOutput = (benchmark: BenchmarkResult): BenchmarkResult => {
+  const outputFile = path.resolve(path.dirname(benchmark.outputFile), `${benchmark.name}.json`);
+  console.log("Transforming output...");
+  nodeProfTransformer(benchmark.outputFile, outputFile);
+  console.log(`Final output written to ${outputFile}`);
+
+  return {...benchmark, outputFile: outputFile};
+};
+
+const normalizeMerlinOutput = (benchmark: BenchmarkResult): BenchmarkResult => {
+  const outputFile = path.resolve(path.dirname(benchmark.outputFile), `${benchmark.name}.json`);
+  console.log("Transforming Merlin output...");
+
+  const merlinContents = fs.readFileSync(benchmark.outputFile).toString()
+  const merlinRawJson = JSON.parse(merlinContents);
+  const callgraph = merlinRawJson['experimentResults'][0]['experimentCallGraph'];
+  fs.writeFileSync(outputFile, JSON.stringify(callgraph, undefined, 2));
+
+  console.log(`Final output written to ${outputFile}`);
+
+  return {...benchmark, outputFile: outputFile};
+};
 
 export const runFullEvaluation = (jamPath: string, nodeProfPath: string, benchmarks: string[]) => {
   const graalNodeHome = getGraalNodePath(nodeProfPath);
 
   generateNodeProfCallGraphs(jamPath, graalNodeHome, benchmarks).map(normalizeNodeProfOutput);
   generateJamCallGraphs(jamPath, graalNodeHome, benchmarks);
+  generateMerlinCallGraphs(benchmarks).map(normalizeMerlinOutput);
 };
